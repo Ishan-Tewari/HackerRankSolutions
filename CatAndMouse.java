135e1ea1e13d7915621a685b7b5a1a4e8393f53f
Ishan Tewari <52094130+Ishan-Tewari@users.noreply.github.com>
2019-07-14 22:29:47 +0530

    Create CatAndMouse.java

---
+++ CatAndMouse.java
@@ -0,0 +1,237 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+
+import static java.lang.Math.*;
+
+// fast reader program for faster reading of data(you can use System.out.println too)
+public class CatAndMouse implements Runnable
+{
+    static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int read()
+        {
+            if (numChars==-1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+
+                if(numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        public int nextInt()
+        {
+            int c = read();
+
+            while(isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do
+            {
+                if(c<'0'||c>'9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.')
+            {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.')
+            {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c))
+                {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+    public static void main(String args[]) throws Exception
+    {
+        new Thread(null, new CatAndMouse(),"Main",1<<27).start();
+    }
+
+    // main logic of program
+    public void run(){
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+
+        //scanning the number of testcases
+        int q=in.nextInt();
+        while (q--!=0){
+
+            //scanning the locations of cat a, cat b, mouse c
+            int x,y,z;
+            x=in.nextInt();
+            y=in.nextInt();
+            z=in.nextInt();
+
+            //calculating the distance of mouse from both cats
+            int catADist,catBDist;
+            catADist=x-z;
+            catBDist=y-z;
+
+            //taking mod of both values(converting negative values to positive values)
+            if(catADist<0)
+                catADist=-catADist;
+            if(catBDist<0)
+                catBDist=-catBDist;
+
+            //if cat a is far away from mouse as compared to cat b, then cat b will reach the mouse first 
+            if(catADist>catBDist)
+                w.println("Cat B");
+            
+            //if cat b is far away from mouse as compared to cat a, then cat a will reach the mouse first
+            else if(catBDist>catADist)
+                w.println("Cat A");
+
+            //if both the cats are at same distance from the mouse, the mouse escapes
+            else
+                w.println("Mouse C");
+        }
+
+        w.flush();
+        w.close();
+    }
+
+}
+
