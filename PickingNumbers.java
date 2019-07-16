ebc9a2b2be7ef460bccf2c4b29a90b5d75b7c41e
Ishan Tewari <52094130+Ishan-Tewari@users.noreply.github.com>
2019-07-17 00:00:55 +0530

    Create PickingNumbers.java

---
+++ PickingNumbers.java
@@ -0,0 +1,243 @@
+//https://www.hackerrank.com/challenges/picking-numbers/problem?h_r=next-challenge&h_v=zen
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+
+import static java.lang.Math.*;
+
+// fast reader program for faster reading of data(you can use System.out.println too)
+public class PickingNumbers implements Runnable
+{
+    static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int read()
+        {
+            if (numChars==-1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+
+                if(numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        public int nextInt()
+        {
+            int c = read();
+
+            while(isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do
+            {
+                if(c<'0'||c>'9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.')
+            {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.')
+            {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c))
+                {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+    public static void main(String args[]) throws Exception
+    {
+        new Thread(null, new PickingNumbers(),"Main",1<<27).start();
+    }
+
+    // main logic of program
+    public void run(){
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+
+        //declaring and initializing array
+        int n=in.nextInt();
+        int a[]=new int[n];
+
+        for (int i=0;i<n;i++){
+            a[i]=in.nextInt();
+        }
+
+        //sorting the array to reduce the test runs
+        Arrays.sort(a);
+        int len=a.length;
+        int max=0;
+        int sameElement=0;
+
+
+
+        for(int i=len-1;i>=0;i--){
+            int count=1;
+
+            //if the element is same, we can skip the iteration, which will reduce the running time
+            if(a[i]==sameElement){
+                continue;
+            }
+            else{
+                sameElement=a[i];
+            }
+
+            //for element starting from the end, we have to check if the elements before it have diff<=1 with it
+            for(int j=i-1;j>=0;j--){
+                if((a[i]-a[j])<=1){
+                    count++;
+                }
+            }
+            //calculating maximum of count and max
+            max=count>max?count:max;
+        }
+        w.println(max);
+
+        w.flush();
+        w.close();
+    }
+
+}
+
