e0699b8a124eb3c2fa35e01f37c9266ff59b93ec
Ishan Tewari <52094130+Ishan-Tewari@users.noreply.github.com>
2019-07-29 23:14:22 +0530

    Create BeautifulDays.java

---
+++ BeautifulDays.java
@@ -0,0 +1,236 @@
+//https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem?h_r=next-challenge&h_v=zen
+//for the main logic of the program, skip to line 195
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+// fast reader program for faster reading of data(you can use System.out.println too)
+public class BeautifulDays implements Runnable
+{
+    static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int read()
+        {
+            if (numChars==-1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+
+                if(numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        public int nextInt()
+        {
+            int c = read();
+
+            while(isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do
+            {
+                if(c<'0'||c>'9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.')
+            {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.')
+            {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c))
+                {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+    public static void main(String args[]) throws Exception
+    {
+        new Thread(null, new BeautifulDays(),"Main",1<<27).start();
+    }
+
+    // main logic of program
+    public void run(){
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+
+        //declaring and initializing i,j,k
+        //declaring and initializing count to 0
+        int count=0;
+        int i = in.nextInt();
+        int j = in.nextInt();
+        int k = in.nextInt();
+
+        //to repeat the process from i to j
+        for(int loopVar=i;loopVar<=j;loopVar++) {
+
+            // taking original number as this intance of loop variable
+            // initializing mirrorNumber to 0
+            // taking a temp variable and assigning the value of the number of this instance(loopVar)
+            int  originalNumber = loopVar, mirrorNumber = 0,temp = loopVar;
+
+            //finding the mirror image of the number
+            while (temp > 0) {
+                int r = temp % 10;
+                mirrorNumber = (mirrorNumber * 10) + r;
+                temp = temp / 10;
+            }
+            
+            //taking the absoulte difference between the original number and the mirror number
+            int absoulteDiff = Math.abs(originalNumber-mirrorNumber);
+
+            //if the absoluteDiff%k==0 then the day is beautiful
+            if(absoulteDiff%k==0)
+                count++;
+        }
+
+        //printing the number of days
+        w.println(count);
+
+        w.flush();
+        w.close();
+    }
+
+}
+
