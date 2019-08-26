7fbf201cca965047c24e6f08e093278912d538d7
Ishan Tewari <52094130+Ishan-Tewari@users.noreply.github.com>
2019-08-26 12:58:41 +0530

    Create JumpingOnTheCloudsRevisited.java

---
+++ JumpingOnTheCloudsRevisited.java
@@ -0,0 +1,223 @@
+//https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?h_r=next-challenge&h_v=zen
+//for the main logic of the program, skip to line 195
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class JumpingOnTheCloudsRevisited implements Runnable
+{
+    static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int read()
+        {
+            if (numChars==-1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+
+                if(numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        public int nextInt()
+        {
+            int c = read();
+
+            while(isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do
+            {
+                if(c<'0'||c>'9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.')
+            {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.')
+            {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c))
+                {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+    public static void main(String args[]) throws Exception
+    {
+        new Thread(null, new JumpingOnTheCloudsRevisited(),"Main",1<<27).start();
+    }
+
+    public void run()
+    {
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+
+        int numOfClouds = in.nextInt();
+        int sizeOfJump = in.nextInt();
+        int e = 100;
+        int c[] = new int[numOfClouds];
+        for (int i=0;i<numOfClouds;i++){
+            c[i] = in.nextInt();
+        }
+
+        for (int i=0; i<numOfClouds; i++){
+            if (c[(i+sizeOfJump) % numOfClouds] == 1)
+                e -= 3;
+            else
+                e -= 1;
+
+            //incrementing i by the sizeOfJump amount(in a circular manner) -1 as it'll again increment in the for loop
+            i=(i+sizeOfJump) % numOfClouds - 1;
+            if (i+1==0)
+                break;
+
+        }
+        w.println(e);
+
+        w.flush();
+        w.close();
+    }
+}
+
